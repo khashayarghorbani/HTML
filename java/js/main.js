30ede2360b3630127766ddfd89743245b46490f3
Khashayar <khashayar.ghorbani@itdcanada.ca>
2019-08-12 12:28:48 -0700

    Add files via upload

---
+++ java/js/main.js
@@ -0,0 +1,13 @@
+
+
+var date = new Date();
+//GET ELEMENT BY ID EXAMPLE
+document.getElementById('Fromjs').innerHTML= date;
+
+//ALERT EXAMPLE
+alert("Now I have JS")
+
+//GET ELEMENT BY CLASS EXAMPLE
+document.getElementsByClassName('myclass')[0].innerHTML="These are my classes";
+document.getElementsByClassName('myclass')[2].innerHTML="These are my classes";
+document.getElementsByClassName('myclass')[3].innerHTML="These are my classes";
